17737e90fbe5c63938d75255127bc72ef3a20937
ykwksxzq <132530452+ykwksxzq@users.noreply.github.com>
2024-01-25 15:02:53 +0000

    deviseのコントローラーの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,20 @@
 Rails.application.routes.draw do
-  devise_for :admins
-  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+# 管理者用
+# URL /admin/sign_in ...
+devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
+  sessions: "admin/sessions"
+}
+
+# 顧客用
+# URL /users/sign_in ...
+devise_for :users,skip: [:passwords], controllers: {
+  registrations: "public/registrations",
+  sessions: 'public/sessions'
+}
+
 end
