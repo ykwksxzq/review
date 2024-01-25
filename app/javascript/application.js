15f0e9c7d0400c452e0b1a586aa50f471b472421
ykwksxzq <132530452+ykwksxzq@users.noreply.github.com>
2024-01-25 19:16:32 +0000

    adminのログイン機能

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,8 +1,1 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-
-//= require jquery3
-//= require popper
-//= require bootstrap
-
-import "@hotwired/turbo-rails"
-import "controllers"
