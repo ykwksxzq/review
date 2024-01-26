067da523e51c5de53aba8991f219bfe41516805c
ykwksxzq <132530452+ykwksxzq@users.noreply.github.com>
2024-01-26 19:30:19 +0000

    ジャンル登録機能

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -2,28 +2,36 @@ class Admin::GenresController < ApplicationController
   before_action :authenticate_admin!
 
   def index
-    @shop = Shop.new
-    @shops = Shop.all
+    @genre = Genre.new
+    @genres = Genre.all
   end
 
   def create
-    @shop = Shop.new(shop_params)
-    @shop.save
-    redirecto_to admin_shops_path
+    @genre = Genre.new(genre_params)
+    @genre.save
+    redirect_to admin_genres_path
   end
 
+  def edit
+    @genre = Genre.find(params[:id])
+  end
 
+  def update
+    @genre = Genre.find(params[:id])
+    @genre.update(genre_params)
+    redirect_to admin_genres_path
+  end
 
-
-
-
-
-
+  def destroy
+    @genre = Genre.find(params[:id])
+    @genre.destroy
+    redirect_to admin_genres_path
+  end
 
   private
 
-  def shop_params
-    params.require(:shop).permit(:name)
+  def genre_params
+    params.require(:genre).permit(:name)
   end
 
 end
--- app/models/genre.rb
+++ app/models/genre.rb
@@ -2,4 +2,6 @@ class Genre < ApplicationRecord
   
   has_many :posts, dependent: :destroy
   
+  validates :name, presence: true, uniqueness: true
+  
 end
