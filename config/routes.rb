983bff12933fe6c62ec74ee115c3d052f6234533
ykwksxzq <132530452+ykwksxzq@users.noreply.github.com>
2024-01-26 07:13:12 +0000

    userのログイン機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,13 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
 
+# 顧客用
+# URL /users/sign_in ...
+devise_for :users,skip: [:passwords], controllers: {
+  registrations: "public/registrations",
+  sessions: 'public/sessions'
+}
+
 # 管理者用
 # URL /admin/sign_in ...
 devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
@@ -13,15 +20,6 @@ devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
 namespace :admin do
   get 'top' => 'homes#top', as: ''
 end
-
-
-
-# 顧客用
-# URL /users/sign_in ...
-devise_for :users,skip: [:passwords], controllers: {
-  registrations: "public/registrations",
-  sessions: 'public/sessions'
-}
 
 scope module: :public do
   root to: "homes#top"
