c2f9a3ea299f7fc2e755d2c8bed483ad3ecaf23a
ykwksxzq <132530452+ykwksxzq@users.noreply.github.com>
2024-02-11 13:17:40 +0000

    投稿編集機能

--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -19,18 +19,28 @@ end
 
 def index
   @posts = Post.all
+  @tag_list = Tag.all
 end
 
 def show
   @post = Post.find(params[:id])
 end
 
+def edit
+  @post = Post.find(params[:id])
+end
+
+def update
+  @post = Post.find(params[:id])
+  @post.update(post_params)
+  redirect_to post_path(@post.id)
+end
 
 
 private
 
 def post_params
-  params.require(:post).permit(:user_id, :genre_id, :shop_id, :title, :content, :price, :rating, :status, :image)
+  params.require(:post).permit(:user_id, :genre_id, :shop_id, :title, :content, :price, :rating, :status, :image, tag_ids: [])
 end
 
 
